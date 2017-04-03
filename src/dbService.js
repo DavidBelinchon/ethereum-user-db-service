a36bf075f2bb7e191dcae5f21ea5bd1c7488513a
iozi <ivo.zielinski@gft.com>
2017-04-03 12:45:24 +0200

    Added pooled connections

--- src/dbService.js
+++ src/dbService.js
@@ -1,115 +1,104 @@
-/** 
-* The MIT License (MIT) 
-*  
-* Copyright (c) 2016 Auth0, Inc. <support@auth0.com> (http://auth0.com) 
-*  
-* Permission is hereby granted, free of charge, to any person obtaining a copy 
-* of this software and associated documentation files (the "Software"), to deal 
-* in the Software without restriction, including without limitation the rights 
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-* copies of the Software, and to permit persons to whom the Software is 
-* furnished to do so, subject to the following conditions: 
-*  
-* The above copyright notice and this permission notice shall be included in all 
-* copies or substantial portions of the Software. 
-*  
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
-* SOFTWARE. 
-*/
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Auth0, Inc. <support@auth0.com> (http://auth0.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 'use strict';
 
 const Q = require('q'),
-      mysql = require('mysql');
+    mysql = require('mysql');
 
 module.exports = DbService;
 
-var connection;
+var pool;
 
 function DbService(host, database, user, password) {
 
-  var config = {
-    host: host ? host : 'localhost',
-    user: user ? user : 'root',
-    password: password ? password : 'my-secret-pw',
-    database: database ? database : 'service-users'
-  }
-
-  connection = mysql.createConnection(config);
-
-  function handleDisconnect(client) {
-    client.on('error', function (error) {
-      if (!error.fatal) return;
-      if (error.code !== 'PROTOCOL_CONNECTION_LOST') throw err;
-
-      console.error('> Re-connecting lost MySQL connection: ' + error.stack);
-
-      connection = mysql.createConnection(config);
-      handleDisconnect(connection);
+    pool = mysql.createPool({
+        connectionLimit: 10,
+        host: host ? host : 'localhost',
+        user: user ? user : 'root',
+        password: password ? password : 'my-secret-pw',
+        database: database ? database : 'service-users'
     });
-  }
-  handleDisconnect(connection);
 }
 
 DbService.prototype.checkIfMobileMappingTableExists = function checkIfMobileMappingTableExists() {
-  return promisify("SHOW TABLES LIKE 'mobileMapping';");
+    return promisify("SHOW TABLES LIKE 'mobileMapping';");
 }
 
 DbService.prototype.createMobileMappingTable = function createMobileMappingTable() {
-  return promisify("CREATE TABLE IF NOT EXISTS mobileMapping (secondaryAddress VARCHAR(100) PRIMARY KEY, registrationToken VARCHAR(256));");
+    return promisify("CREATE TABLE IF NOT EXISTS mobileMapping (secondaryAddress VARCHAR(100) PRIMARY KEY, registrationToken VARCHAR(256));");
 }
 
-DbService.prototype.insertMapping = function insertMapping(secondaryAddress,registrationToken) {
-  return promisify("INSERT INTO mobileMapping SET ?",{secondaryAddress:secondaryAddress,registrationToken:registrationToken});
+DbService.prototype.insertMapping = function insertMapping(secondaryAddress, registrationToken) {
+    return promisify("INSERT INTO mobileMapping SET ?", {
+        secondaryAddress: secondaryAddress,
+        registrationToken: registrationToken
+    });
 }
 
 DbService.prototype.getMappings = function getMappings() {
-  return promisify("SELECT * FROM mobileMapping");
+    return promisify("SELECT * FROM mobileMapping");
 }
 
 DbService.prototype.getMappingByAddress = function getMappingByAddress(secondaryAddress) {
-  return promisify("SELECT * FROM mobileMapping WHERE `secondaryAddress` = ?",[secondaryAddress]);
+    return promisify("SELECT * FROM mobileMapping WHERE `secondaryAddress` = ?", [secondaryAddress]);
 }
 
 DbService.prototype.checkIfUserCredentialsTableExists = function checkIfUserCredentialsTableExists() {
-  return promisify("SHOW TABLES LIKE 'userCredentials';");
+    return promisify("SHOW TABLES LIKE 'userCredentials';");
 }
 
 DbService.prototype.createUserCredentialsTable = function createUserCredentialsTable() {
-  return promisify("CREATE TABLE IF NOT EXISTS userCredentials (email VARCHAR(100) PRIMARY KEY, primaryAddress VARCHAR(100));");
+    return promisify("CREATE TABLE IF NOT EXISTS userCredentials (email VARCHAR(100) PRIMARY KEY, primaryAddress VARCHAR(100));");
 }
 
-DbService.prototype.insertUserCredential = function insertUserCredential(email,primaryAddress) {
-  return promisify("INSERT INTO userCredentials SET ?",{email:email,primaryAddress :primaryAddress});
+DbService.prototype.insertUserCredential = function insertUserCredential(email, primaryAddress) {
+    return promisify("INSERT INTO userCredentials SET ?", {email: email, primaryAddress: primaryAddress});
 }
 
 DbService.prototype.getUserCredentials = function getUserCredentials() {
-  return promisify("SELECT * FROM userCredentials");
+    return promisify("SELECT * FROM userCredentials");
 }
 
 DbService.prototype.getUserCredentialsByEmail = function getUserCredentialsByEmail(email) {
-  return promisify("SELECT * FROM userCredentials WHERE `email` = ?",[email]);
+    return promisify("SELECT * FROM userCredentials WHERE `email` = ?", [email]);
 }
 
-function promisify(query,params) {
-  var deferred = Q.defer();
-  var promise = Q.fcall(function() {
-    var callback = function(error,result){
-      if(error) {
-        deferred.reject(error);
-      } else {
-        deferred.resolve(result);
-      }
-    }
-    if(params) {
-      connection.query(query,params,callback);
-    } else {
-      connection.query(query,callback);
-    }
-  });
-  return deferred.promise;
+function promisify(query, params) {
+    var deferred = Q.defer();
+    Q.fcall(function () {
+        var callback = function (error, result) {
+            if (error) {
+                deferred.reject(error);
+            } else {
+                deferred.resolve(result);
+            }
+        }
+        if (params) {
+            pool.query(query, params, callback);
+        } else {
+            pool.query(query, callback);
+        }
+    });
+    return deferred.promise;
 }
--- test/dbServiceIntegrationTest.js
+++ test/dbServiceIntegrationTest.js
@@ -1,26 +1,26 @@
-/** 
-* The MIT License (MIT) 
-*  
-* Copyright (c) 2016 Auth0, Inc. <support@auth0.com> (http://auth0.com) 
-*  
-* Permission is hereby granted, free of charge, to any person obtaining a copy 
-* of this software and associated documentation files (the "Software"), to deal 
-* in the Software without restriction, including without limitation the rights 
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-* copies of the Software, and to permit persons to whom the Software is 
-* furnished to do so, subject to the following conditions: 
-*  
-* The above copyright notice and this permission notice shall be included in all 
-* copies or substantial portions of the Software. 
-*  
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
-* SOFTWARE. 
-*/
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Auth0, Inc. <support@auth0.com> (http://auth0.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 'use strict';
 
 const assert = require("chai").assert,
@@ -40,56 +40,97 @@ const assert = require("chai").assert,
 
 var dbService = new DbService(TEST_DB_HOST, TEST_DB_NAME);
 
-describe("DB - Integration tests", function() {
+describe("DBBBB - Integration tests", function () {
 
-    it("should create mobile_mapping table and check if this table exists", function() {
-        return dbService.createMobileMappingTable().then(function(result){
-            return dbService.checkIfMobileMappingTableExists().then(function(checkResult){
-                assert.lengthOf(checkResult, 1);
+    it("should create mobile_mapping table and check if this table exists", function () {
+        this.timeout(10000);
+        return dbService.createMobileMappingTable()
+            .then(function() {
+                return dbService.checkIfMobileMappingTableExists()
+            })
+            .then(function verifyResult(result) {
+                console.log(result)
+                assert.lengthOf(result, 1);
+            }).fail(function onFailure(error) {
+                console.log("ERROR:" + error.stack)
             });
-        });
     });
-    it("should check if single mobile_mapping is inserted into table", function() {
-        return dbService.insertMapping(SAMPLE_SECONDARY_ADDRESS, SAMPLE_REGISTRATION_TOKEN).then(function(result){
-            return dbService.getMappingByAddress(SAMPLE_SECONDARY_ADDRESS).then(function(getResult){
-                assert.equal(getResult[0].secondaryAddress, SAMPLE_SECONDARY_ADDRESS);
-                assert.equal(getResult[0].registrationToken, SAMPLE_REGISTRATION_TOKEN);
+
+    it("should check if single mobile_mapping is inserted into table", function () {
+        this.timeout(10000);
+        return dbService.insertMapping(SAMPLE_SECONDARY_ADDRESS, SAMPLE_REGISTRATION_TOKEN)
+            .then(function() {
+                return dbService.getMappingByAddress(SAMPLE_SECONDARY_ADDRESS)
+            })
+            .then(function verifyResult(result) {
+                console.log(result)
+                assert.equal(result[0].secondaryAddress, SAMPLE_SECONDARY_ADDRESS);
+                assert.equal(result[0].registrationToken, SAMPLE_REGISTRATION_TOKEN);
+            }).fail(function onFailure(error) {
+                console.log("ERROR:" + error.stack)
             });
-        });
     });
-    it("should check if there are 2 mobile_mappings inserted into table", function() {
-        return dbService.insertMapping(SAMPLE_SECONDARY_ADDRESS_2, SAMPLE_REGISTRATION_TOKEN_2).then(function(result){
-            return dbService.getMappings().then(function(getResults){
-                assert.lengthOf(getResults, 2);
+
+    it("should check if there are 2 mobile_mappings inserted into table", function () {
+        this.timeout(10000);
+        return dbService.insertMapping(SAMPLE_SECONDARY_ADDRESS_2, SAMPLE_REGISTRATION_TOKEN_2)
+            .then(function() {
+                return dbService.getMappings()
+            })
+            .then(function verifyResult(result) {
+                assert.lengthOf(result, 2);
+                console.log(result)
                 //Below asserts are with '||' because order of inserting data is not certain
-                assert.equal(getResults[1].secondaryAddress == SAMPLE_SECONDARY_ADDRESS || getResults[1].secondaryAddress == SAMPLE_SECONDARY_ADDRESS_2, true);
-                assert.equal(getResults[1].registrationToken == SAMPLE_REGISTRATION_TOKEN || getResults[1].registrationToken == SAMPLE_REGISTRATION_TOKEN_2, true);
+                assert.equal(result[1].secondaryAddress == SAMPLE_SECONDARY_ADDRESS || result[1].secondaryAddress == SAMPLE_SECONDARY_ADDRESS_2, true);
+                assert.equal(result[1].registrationToken == SAMPLE_REGISTRATION_TOKEN || result[1].registrationToken == SAMPLE_REGISTRATION_TOKEN_2, true);
+            }).fail(function onFailure(error) {
+                console.log("ERROR:" + error.stack)
             });
-        });
     });
-    it("should create user_credentials table and check if this table exists", function() {
-        return dbService.createUserCredentialsTable().then(function(result){
-            return dbService.checkIfUserCredentialsTableExists().then(function(checkResult){
-                assert.lengthOf(checkResult, 1);
+
+    it("should create user_credentials table and check if this table exists", function () {
+        this.timeout(10000);
+        return dbService.createUserCredentialsTable()
+            .then(function() {
+                return dbService.checkIfUserCredentialsTableExists()
+            })
+            .then(function verifyResult(result) {
+                console.log(result)
+                assert.lengthOf(result, 1);
+            }).fail(function onFailure(error) {
+                console.log("ERROR:" + error.stack)
             });
-        });
     });
-    it("should check if single user_credential is inserted into table", function() {
-        return dbService.insertUserCredential(SAMPLE_EMAIL, SAMPLE_PRIMARY_ADDRESS).then(function(result){
-            return dbService.getUserCredentialsByEmail(SAMPLE_EMAIL).then(function(getResult){
-                assert.equal(getResult[0].email, SAMPLE_EMAIL);
-                assert.equal(getResult[0].primaryAddress, SAMPLE_PRIMARY_ADDRESS);
+
+    it("should check if single user_credential is inserted into table", function () {
+        this.timeout(10000);
+        return dbService.insertUserCredential(SAMPLE_EMAIL, SAMPLE_PRIMARY_ADDRESS)
+            .then(function() {
+                return dbService.getUserCredentialsByEmail(SAMPLE_EMAIL)
+            })
+            .then(function verifyResult(result) {
+                console.log(result)
+                assert.equal(result[0].email, SAMPLE_EMAIL);
+                assert.equal(result[0].primaryAddress, SAMPLE_PRIMARY_ADDRESS);
+            }).fail(function onFailure(error) {
+                console.log("ERROR:" + error.stack)
             });
-        });
     });
-    it("should check if there are 2 user_credentials inserted into table", function() {
-        return dbService.insertUserCredential(SAMPLE_EMAIL_2, SAMPLE_PRIMARY_ADDRESS_2).then(function(result){
-            return dbService.getUserCredentials().then(function(getResults){
-                assert.lengthOf(getResults, 2);
+
+    it("should check if there are 2 user_credentials inserted into table", function () {
+        this.timeout(10000);
+        return dbService.insertUserCredential(SAMPLE_EMAIL_2, SAMPLE_PRIMARY_ADDRESS_2)
+            .then(function() {
+                return dbService.getUserCredentials()
+            })
+            .then(function verifyResult(result) {
+                console.log(result)
+                assert.lengthOf(result, 2);
                 //Below asserts are with '||' because order of inserting data is not certain
-                assert.equal(getResults[1].email == SAMPLE_EMAIL || getResults[1].email == SAMPLE_EMAIL_2, true);
-                assert.equal(getResults[1].primaryAddress == SAMPLE_PRIMARY_ADDRESS || getResults[1].primaryAddress == SAMPLE_PRIMARY_ADDRESS_2, true);
+                assert.equal(result[1].email == SAMPLE_EMAIL || result[1].email == SAMPLE_EMAIL_2, true);
+                assert.equal(result[1].primaryAddress == SAMPLE_PRIMARY_ADDRESS || result[1].primaryAddress == SAMPLE_PRIMARY_ADDRESS_2, true);
+            }).fail(function onFailure(error) {
+                console.log("ERROR:" + error.stack)
             });
-        });
     });
-});
+})
